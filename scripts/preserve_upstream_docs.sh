7649631a6af6becdf210908d7ea7f78894555c8e
Eric Theise <erictheise@gmail.com>
2024-09-15 14:43:08 +0200

    An upstream merge Action.

--- scripts/preserve_upstream_docs.sh
+++ scripts/preserve_upstream_docs.sh
@@ -3,13 +3,13 @@
 if [ ! -d "upstream" ]; then
   mkdir upstream
 fi
-for i in $(git ls-tree upstream/master --name-only); do
+for i in $(git ls-tree upstream/main --name-only); do
   if [[ $i == *.md ]];then
     echo $i
-    git show upstream/master:$i > upstream/$i
+    git show upstream/main:$i > upstream/$i
   fi
 done
-git show upstream/master:LICENSE > upstream/LICENSE
+git show upstream/main:LICENSE > upstream/LICENSE
 git add upstream
 if [ -n "$(git status --porcelain)" ]; then
   git commit -m 'Preserving upstream markdown & text files.'
